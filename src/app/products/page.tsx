44dbe0d50a0ec50e0bfe02366e12546c2d56afc1
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2025-02-18 01:18:47 +0530

    Contact form

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,6 +1,9 @@
+import Appbar from "@/components/Appbar";
+
 export default function Products() {
     return (
         <div>
+            <Appbar/>
             <h1>Products</h1>
         </div>
     )
